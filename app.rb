8db37bb5f7781637945b4fdbeade67c2ee12cf9e
Dana Palmer <roiai4@hotmail.com>
2019-03-04 23:50:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+<<<<<<< HEAD
   get '/' do
+=======
+  get '/' do 
+>>>>>>> 22014b02814e8ca67e560309835a4fe03476eade
     "My new server with Shotgun!"
   end
 
